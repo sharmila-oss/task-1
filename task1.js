d29c2c2383cd245077ce0c37da2ac8efff0afe83
sharmila-oss <ks3.sharmila@gmail.com>
2026-04-06 21:37:28 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,24 @@
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(5, 3));
+
+const square = (n) => {
+    return n * n;
+};
+console.log(square(4));
+
+const hello = () => {
+    console.log("Hello World");
+};
+hello();
+
+const greet = (name) => {
+    console.log("HI " + name);
+};
+greet("Sharmila");
+
+const multiply = (a, b) => {
+    console.log("Multiplication: " + (a * b));
+};
+multiply(4, 5);
